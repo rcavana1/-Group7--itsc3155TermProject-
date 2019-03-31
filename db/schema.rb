89865a93e02966108004cced99e6be3235af9527
Drew Cavanaugh <rcavana1@uncc.edu>
2019-03-31 07:36:00 +0000

    Create Account model & ran db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_044513) do
+ActiveRecord::Schema.define(version: 2019_03_31_073417) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "budgets", force: :cascade do |t|
     t.date "month"
