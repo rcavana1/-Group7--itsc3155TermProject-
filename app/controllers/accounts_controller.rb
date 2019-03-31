ca42f1e7c64d7eb6d9ab80c3f5f2f5f494e2e860
Drew Cavanaugh <rcavana1@uncc.edu>
2019-03-31 08:34:09 +0000

    Build foundation for Account new and show views

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,6 +5,13 @@ class AccountsController < ApplicationController
     def new
     end
     
+    def create
+        @account = Account.new(account_params)
+        
+        @account.save
+        redirect_to @account
+    end
+    
     def edit
     end
     
@@ -12,5 +19,11 @@ class AccountsController < ApplicationController
     end
     
     def show
+        @account = Account.find(params[:id])
     end
 end
+
+private
+    def account_params
+        params.require(:account).permit(:username, :password, :name)
+    end
